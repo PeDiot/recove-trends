ed35b67202be99e676d4ba8685ea71e917d4e9ab
PeDiot <p.emmanuel.diot@gmail.com>
2026-03-23 11:03:57 +0100

    use python-decouple for env variables, enhance dbt job handling, and improve query formatting. Update configuration parameters and add new functions for payload creation and query retrieval.

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -1,7 +1,8 @@
-from typing import List
+from typing import List, Dict
 from pydantic import BaseModel
 
-import os, time, requests, json
+import time, requests, json
+from decouple import config
 
 from dagster import Config, get_dagster_logger, job, op, ScheduleDefinition
 
@@ -11,22 +12,26 @@ from google.oauth2 import service_account
 from slack_sdk import WebClient
 
 
-DBT_ACCOUNT_ID = os.getenv("DBT_ACCOUNT_ID")
-DBT_JOB_ID = os.getenv("DBT_JOB_ID")
-DBT_API_TOKEN = os.getenv("DBT_API_TOKEN")
+DBT_API_URL = "https://ar616.us1.dbt.com/api/v2/accounts"
+DBT_ACCOUNT_ID = config("DBT_ACCOUNT_ID")
+DBT_JOB_ID = config("DBT_JOB_ID")
+DBT_API_TOKEN = config("DBT_API_TOKEN")
+DBT_JOB_STATUS_SUCCESS = 10
+DBT_JOB_STATUS_FAILED = 20
 
-SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
-SLACK_CHANNEL = os.getenv("SLACK_CHANNEL")
+SLACK_BOT_TOKEN = config("SLACK_BOT_TOKEN")
+SLACK_CHANNEL = config("SLACK_CHANNEL")
 
-GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
-GCP_DATASET_ID = os.getenv("GCP_DATASET_ID")
-GCP_CREDENTIALS_JSON = os.getenv("GCP_CREDENTIALS_JSON")
+GCP_PROJECT_ID = config("GCP_PROJECT_ID")
+GCP_DATASET_ID = config("GCP_DATASET_ID")
+GCP_CREDENTIALS_JSON = config("GCP_CREDENTIALS_JSON")
 
 
 class TrendsConfig(Config):
-    ngrams_n: int = 2
-    top_k: int = 50
-    lookback_hours: int = 24
+    ngrams_n: int = 3
+    top_k: int = 10
+    lookback_hours: int = 48
+    normalize_score: bool = False
 
 
 class Trend(BaseModel):
@@ -35,19 +40,32 @@ class Trend(BaseModel):
     num_click_out_events: int
     num_save_events: int
     num_converting_users: int
-    engagement_score: int
+    engagement_score: float
 
     def to_message(self, rank: int) -> str:
         return (
-            f"{rank}. *{self.ngram}* — Score: {self.engagement_score} "
-            f"({self.num_search_query_events} searches; "
-            f"{self.num_click_out_events} click-outs; "
-            f"{self.num_save_events} saves; "
-            f"{self.num_converting_users} converting users)\n"
+            f"{rank}. *{self.ngram}* — Score: {self.engagement_score:,}\n"
+            f"    • {self.num_search_query_events:,} searches\n"
+            f"    • {self.num_click_out_events:,} click-outs\n"
+            f"    • {self.num_save_events:,} saves\n"
+            f"    • {self.num_converting_users:,} users\n\n"
         )
 
 
-def create_bigquery_client() -> bigquery.Client:
+def create_dbt_payload(config: TrendsConfig) -> Dict:
+    dbt_vars = {
+        "ngrams_n": config.ngrams_n,
+        "lookback_hours": config.lookback_hours,
+        "top_k": config.top_k,
+    }
+
+    vars_str = json.dumps(dbt_vars)
+    dbt_command = f"dbt build --select dynamic_trends_ngrams --vars '{vars_str}'"
+
+    return {"cause": "Triggered by Dagster", "steps_override": [dbt_command]}
+
+
+def initialize_bq_client() -> bigquery.Client:
     creds_dict = json.loads(GCP_CREDENTIALS_JSON)
     credentials = service_account.Credentials.from_service_account_info(creds_dict)
 
@@ -57,32 +75,76 @@ def create_bigquery_client() -> bigquery.Client:
     )
 
 
-@op
-def run_dbt_cloud_job() -> bool:
-    logger = get_dagster_logger()
-    headers = {"Authorization": f"Token {DBT_API_TOKEN}"}
-
-    trigger_url = f"https://cloud.getdbt.com/api/v2/accounts/{DBT_ACCOUNT_ID}/jobs/{DBT_JOB_ID}/run/"
-    response = requests.post(
-        trigger_url, headers=headers, json={"cause": "Triggered by Dagster"}
+def create_bq_retrieval_query(config: TrendsConfig) -> str:
+    table_name = (
+        f"trends_{config.ngrams_n}grams_{config.lookback_hours}h_top{config.top_k}"
     )
+
+    return f"""
+SELECT
+ngram,
+num_search_query_events,
+num_click_out_events,
+num_save_events,
+num_converting_users,
+engagement_score,
+normalized_score
+FROM `{GCP_PROJECT_ID}.{GCP_DATASET_ID}.{table_name}`
+ORDER BY engagement_score DESC
+    """
+
+
+def format_lookback_hours(lookback_hours: int) -> str:
+    if lookback_hours > 24:
+        days = lookback_hours // 24
+        return f"{days} days"
+
+    return f"{lookback_hours} hours"
+
+
+@op
+def trigger_dbt_job(config: TrendsConfig) -> int:
+    logger = get_dagster_logger()
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {DBT_API_TOKEN}",
+    }
+
+    payload = create_dbt_payload(config)
+    url = f"{DBT_API_URL}/{DBT_ACCOUNT_ID}/jobs/{DBT_JOB_ID}/run/"
+    response = requests.post(url, headers=headers, json=payload)
+
     logger.info(response.text)
     response.raise_for_status()
+
     run_id = response.json()["data"]["id"]
+    logger.info(f"Successfully triggered dbt run {run_id}.")
 
-    logger.info(f"Started dbt run {run_id}. Waiting for completion...")
+    return run_id
 
-    status_url = (
-        f"https://cloud.getdbt.com/api/v2/accounts/{DBT_ACCOUNT_ID}/runs/{run_id}/"
-    )
+
+@op
+def wait_for_dbt_job(run_id: int) -> bool:
+    logger = get_dagster_logger()
+
+    headers = {
+        "Accept": "application/json",
+        "Authorization": f"Bearer {DBT_API_TOKEN}",
+    }
+
+    logger.info(f"Waiting for completion of dbt run {run_id}...")
+    url = f"{DBT_API_URL}/{DBT_ACCOUNT_ID}/runs/{run_id}/"
+
     while True:
-        status_res = requests.get(status_url, headers=headers).json()
+        status_res = requests.get(url, headers=headers).json()
         status = status_res["data"]["status"]
 
-        if status == 10:
+        if status == DBT_JOB_STATUS_SUCCESS:
             logger.info("dbt Job Succeeded!")
             return True
-        elif status == 20:
+        elif status == DBT_JOB_STATUS_FAILED:
             raise Exception(f"dbt Job Failed! Check dbt Cloud logs for run {run_id}")
 
         time.sleep(15)
@@ -95,24 +157,8 @@ def fetch_top_trends(config: TrendsConfig, dbt_success: bool) -> List[Trend]:
             "dbt job did not succeed; skipping BigQuery query and Slack notification."
         )
 
-    client = create_bigquery_client()
-
-    table_name = (
-        f"trends_{config.ngrams_n}grams_{config.lookback_hours}h_top{config.top_k}"
-    )
-
-    query = f"""
-    SELECT
-        ngram,
-        num_search_query_events,
-        num_click_out_events,
-        num_save_events,
-        num_converting_users,
-        engagement_score
-    FROM `{GCP_PROJECT_ID}.{GCP_DATASET_ID}.{table_name}`
-    ORDER BY engagement_score DESC
-    LIMIT {config.top_k}
-    """
+    client = initialize_bq_client()
+    query = create_bq_retrieval_query(config, normalize_score=config.normalize_score)
 
     results = client.query(query).result()
 
@@ -135,7 +181,13 @@ def fetch_top_trends(config: TrendsConfig, dbt_success: bool) -> List[Trend]:
 def send_slack_alert(config: TrendsConfig, trends: List[Trend]):
     client = WebClient(token=SLACK_BOT_TOKEN)
 
-    message = f"📈 *Daily Recove Search Trends (Top {config.top_k} - Last {config.lookback_hours}h)*\n\n"
+    lookback_display = format_lookback_hours(config.lookback_hours)
+
+    message = (
+        f"📈 *Top {config.top_k} Recove Trends "
+        f"over the last {lookback_display}*\n\n"
+    )
+
     for i, trend in enumerate(trends, 1):
         message += trend.to_message(i)
 
@@ -145,9 +197,10 @@ def send_slack_alert(config: TrendsConfig, trends: List[Trend]):
 
 @job
 def daily_trends_pipeline():
-    success = run_dbt_cloud_job()
-    trends = fetch_top_trends(success)
-    send_slack_alert(trends)
+    run_id = trigger_dbt_job()
+    success = wait_for_dbt_job(run_id=run_id)
+    trends = fetch_top_trends(dbt_success=success)
+    send_slack_alert(trends=trends)
 
 
 daily_schedule = ScheduleDefinition(
